379807413dfa486887be4e023b22e80f912e2325
Arca-Oexdi <249052355+Arca-Oexdi@users.noreply.github.com>
2026-09-07 17:29:01 -0500

    Aprendiendo python en su sintaxis

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/sebas_dev.py
@@ -0,0 +1,57 @@
+# Comentarios con almuadillas
+# Pero cada una de estas lineas es un comentario
+
+"""
+Lo que esta dentro de estas comillas triples
+Se considera un comentario de multiples lineas
+Donde lo que esta dentro de si mismo, no se ejecuta pero
+Cuenta como un solo comentario de multiples lineas
+"""
+
+'''
+Lo que esta dentro de estas comillas simples triples
+Se considera un comentario de multiples lineas
+Donde lo que esta dentro de si mismo, no se ejecuta
+'''
+
+print("TIPOS DE DATOS EN PYTHON") 
+print()
+
+# Variable
+print("VARIABLES")
+estas_es_una_variable = "Esta es una variable dentro de una cadena de texto"
+print(estas_es_una_variable)
+print()
+
+# Constante
+print("CONSTANTES")
+CONSTANTE = 3.14159265359
+print("El valor de la constante es:", CONSTANTE)
+print()
+
+# Enteros
+print("ENTEROS")
+enteros:int = 24
+print("El valor de la variable entera es:", enteros)
+print()
+
+# Flotantes
+print("FLOTANTES")
+flotantes:float = 23.45
+print("El valor de la variable flotante es:", flotantes)
+print()
+
+# Cadenas
+print("CADENAS")
+cadenas:str = "Esto es un dato tipo cadena"
+print("El valor de la variable cadena es:", cadenas)
+print()
+
+# Booleanos
+print("BOOLEANOS")
+booleanos:bool = True
+print("El valor de la variable booleana es:", booleanos)
+print()
+
+# Hola Mundo
+print("¡Hola, Python!")
